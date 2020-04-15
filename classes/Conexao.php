724b812581ac6a12f67c5b056ebf3a33e9d75ea1
daniel <daniel.augusto930@gmail.com>
2020-04-15 20:45:07 -0300

    modificacao um

---
+++ classes/Conexao.php
@@ -0,0 +1,31 @@
+<?php
+
+class Conexao
+{
+	private $host;
+	private $user;
+	private $password;
+	private $conn;
+
+	public function __construct() {
+		$this->host = 'mysql:host=localhost;dbname=lanclonete_oo';
+		$this->user = 'root';
+		$this->password = '';
+	}
+
+    public function conectar() {
+    	try {
+			$conn = new PDO(
+							$this->host,
+							$this->user, 
+							$this->password
+						);
+
+			return $conn;
+			
+		} catch (PDOException $e) {
+			echo 'Erro na conexao. Erro reportado: ' . $e->getMessage();
+			exit;
+		}
+    }
+}
